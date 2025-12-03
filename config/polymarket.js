dab586c62c747cfb8bf193b6d2c67b423b742ffa
Sajal Bansal <sajalbnl123@gmail.com>
2025-12-03 19:12:43 +0530

    added blockchain config file

--- config/polymarket.js
+++ config/polymarket.js
@@ -1,58 +1,33 @@
 import { ClobClient } from '@polymarket/clob-client';
-import {  POLYMARKET_CLOB_URL, POLYGON_CHAIN_ID } from './env.js';
-import logger from '../utils/logger.js';
+import { POLYMARKET_API_KEY, POLYMARKET_CLOB_URL, POLYGON_CHAIN_ID } from './env.js';
 
+let clobClient = null;
 
-
-// export const initializeClobClient = () => {
-//     try {
-//         clobClient = new ClobClient(
-//             POLYMARKET_CLOB_URL,
-//             POLYGON_CHAIN_ID,
-//             {
-//                 key: POLYMARKET_API_KEY || '',
-//             }
-//         );
-        
-//         console.log('Polymarket CLOB client initialized');
-//         return clobClient;
-//     } catch (error) {
-//         console.error('Error initializing CLOB client:', error);
-//         throw error;
-//     }
-// };
-
-// export const getClobClient = () => {
-//     if (!clobClient) {
-//         return initializeClobClient();
-//     }
-//     return clobClient;
-// };
-export const getClobClient = async (signer, funderAddress) => {
+export const initializeClobClient = () => {
     try {
-        //In general don't create a new API key, always derive or createOrDerive
-        const creds = await getClobCreds(signer);
-        logger.info("creds generated");
-        const clobClient = new ClobClient(
+        clobClient = new ClobClient(
             POLYMARKET_CLOB_URL,
             POLYGON_CHAIN_ID,
-            signer,
-            await creds,
-            0,
-            funderAddress,
+            {
+                key: POLYMARKET_API_KEY || '',
+            }
         );
+        
+        console.log('Polymarket CLOB client initialized');
         return clobClient;
     } catch (error) {
-        logger.error("failed to get clob client");
-        console.error(error);
+        console.error('Error initializing CLOB client:', error);
         throw error;
     }
 };
 
-const getClobCreds = async (signer) => {
-    const creds = new ClobClient(POLYMARKET_CLOB_URL, POLYGON_CHAIN_ID, signer).createOrDeriveApiKey();
-    return creds;
+export const getClobClient = () => {
+    if (!clobClient) {
+        return initializeClobClient();
+    }
+    return clobClient;
 };
+
 // Market sides
 export const SIDES = {
     BUY: 'BUY',
